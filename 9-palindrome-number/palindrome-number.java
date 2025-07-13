cff95ba8c83b8be91b509a7a500902e6773ac9eb
Avantika <106760603+avantika0601@users.noreply.github.com>
2025-07-14 00:54:07 +0530

    Time: 4 ms (100.00%) | Memory: 44.1 MB (48.35%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,15 +1,13 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if(x<0) return false;
-        ArrayList<Integer> numList = new ArrayList<Integer>();
-        
+        int original = x;
+        int rem = 0;
+        int reverse = 0;
         while(x>0){
-            numList.add(x%10);
+            rem = x%10;
             x/=10;
+            reverse = reverse*10 + rem;
         }
-        ArrayList<Integer> reverseNumList = new ArrayList<>(numList);
-        Collections.reverse(reverseNumList);
-        return(numList.equals(reverseNumList));
-
+        return original == reverse;
     }
 }
