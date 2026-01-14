9060ae5646b8a6f12e7d5cd13e284a103141c22a
Avantika <106760603+avantika0601@users.noreply.github.com>
2026-01-14 12:49:48 +0530

    Time: 2 ms (89.32%) | Memory: 48 MB (24.32%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,23 +1,34 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         int n=nums.length;
-        int i=0, j=1;
-        if(n==1)return;
-        while(i<n && j<n){
-            if(nums[i]==0 && nums[j]!=0){
-                int temp=nums[j];
+        // int i=0, j=1;
+        // if(n==1)return;
+        // while(i<n && j<n){
+        //     if(nums[i]==0 && nums[j]!=0){
+        //         int temp=nums[j];
+        //         nums[j]=nums[i];
+        //         nums[i]=temp;
+        //         i++;
+        //         j++;
+        //     }
+        //     else if(nums[i]==0 && nums[j]==0){
+        //         j++;
+        //     }
+        //     else{
+        //         i++;
+        //         j++;
+        //     }
+        // }
+        int j=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0){
                 nums[j]=nums[i];
-                nums[i]=temp;
-                i++;
                 j++;
             }
-            else if(nums[i]==0 && nums[j]==0){
-                j++;
-            }
-            else{
-                i++;
-                j++;
-            }
+        }
+        while(j<n){
+            nums[j]=0;
+            j++;
         }
     }
 }
