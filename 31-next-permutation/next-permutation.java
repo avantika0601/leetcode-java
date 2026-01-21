866d59c674959f2b54d7acccebc26df0b000d433
Avantika <106760603+avantika0601@users.noreply.github.com>
2026-01-21 21:37:07 +0530

    Time: 6 ms (2.26%) | Memory: 45 MB (9.76%) - LeetSync

---
+++ 31-next-permutation/next-permutation.java
@@ -0,0 +1,38 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+        int index = -1;
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                index = i;
+                break;
+            }
+        }
+        if (index == -1) {
+            reverse(nums, 0, n-1);
+            return;
+        } 
+            for (int i = n - 1; i > index; i--) {
+                if (nums[i] > nums[index]) {
+                    System.out.println("i " + i + "index " + index);
+                    swap(nums, i, index);
+                    break;
+                }
+            }
+            reverse(nums, index + 1, n - 1);
+    }
+
+    void reverse(int[] nums, int a, int b) {
+        while (a < b) {
+            swap(nums, a, b);
+            a++;
+            b--;
+        }
+    }
+
+        void swap(int[]nums, int a, int b) {
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+    }
+}
